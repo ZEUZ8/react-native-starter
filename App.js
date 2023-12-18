8fc6eef519d428af9fe4fde8c955bea505a58797
sinan <ptsinan8590@gmail.com>
2023-12-18 23:57:14 +0530

    Explored image handling in native development and implemented the use of adaptive icons in this commit.

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { View } from "react-native";
+import { View,Image} from "react-native";
+const logoImg = require("./assets/adaptive-icon.png")
 
 export default function App(){
   return (
-    <View style={{flex:1, backgroundColor:"plum"}}>
-      <View style={{ width:200,height:200, backgroundColor:"lightblue"}}></View>
-      <View style={{ width:200,height:200, backgroundColor:"lightgreen"}}></View>
+    <View style={{flex:1, backgroundColor:"plum",padding:50}}>
+      {/* <View style={{ width:200,height:200, backgroundColor:"lightblue"}}></View>
+      <View style={{ width:200,height:200, backgroundColor:"lightgreen"}}></View> */}
+      <Image source={logoImg} style={{width:300, height:300}}/>
+      <Image source={logoImg} style={{width:300, height:300}}/>
     </View>
   )
 }
