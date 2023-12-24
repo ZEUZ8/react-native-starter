518fbf9bb3fe2328c631f06b83375efdfb5c0d89
sinan <ptsinan8590@gmail.com>
2023-12-24 17:12:58 +0530

    Implemented ImageBackground and ScrollView components, covering two major native topics in this commit.

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
-import { View,Image} from "react-native";
-const logoImg = require("./assets/adaptive-icon.png")
+import { View, Image, ImageBackground, Text, ScrollView } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 
-export default function App(){
+export default function App() {
   return (
-    <View style={{flex:1, backgroundColor:"plum",padding:50}}>
+    <View>
+      <ScrollView style={{ flex: 1, backgroundColor: "plum", padding: 50 }}>
       {/* <View style={{ width:200,height:200, backgroundColor:"lightblue"}}></View>
       <View style={{ width:200,height:200, backgroundColor:"lightgreen"}}></View> */}
-      <Image source={logoImg} style={{width:300, height:300}}/>
-      <Image source={logoImg} style={{width:300, height:300}}/>
+      {/* <Image source={logoImg} style={{width:300, height:300}}/> */}
+      <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      <Text>
+        using the image background with the view section using the image
+        background with the view section using the image background with the
+        view section using the image background with the view section using the
+        image background with the view section using the image background with
+        using the image background with the view section using the image
+        background with the view section using the image background with the
+        view section using the image background with the view section using the
+        image background with the view section using the image background with
+        using the image background with the view section using the image
+        background with the view section using the image background with the
+        view section using the image background with the view section using the
+        image background with the view section using the image background with
+        the view section using the image background with the view section{" "}
+      </Text>
+
+      {/* <ImageBackground source={logoImg} style={{flex:1}}>
+        <Text>the who's ready to sacrifice himself for bringin the justice </Text>
+      </ImageBackground> */}
+      <Image source={logoImg} style={{ width: 300, height: 300 }} />
+    </ScrollView>
     </View>
-  )
+  );
 }
-
-
-
