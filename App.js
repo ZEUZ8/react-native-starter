6e4a75e078683c51828e56a3ebb1edeb0bd8d95f
sinan <ptsinan8590@gmail.com>
2024-03-21 20:45:42 +0530

    Implemented modal functionality in React Native, completing basic design and functionality using state management.

--- App.js
+++ App.js
@@ -1,35 +1,26 @@
-import { View, Image, ImageBackground, Text, ScrollView } from "react-native";
+import { useState } from "react";
+import { View, Image, ImageBackground, Text, ScrollView ,Modal, Button} from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [modal,setModal] = useState(false)
   return (
-    <View>
-      <ScrollView style={{ flex: 1, backgroundColor: "plum", padding: 50 }}>
-      {/* <View style={{ width:200,height:200, backgroundColor:"lightblue"}}></View>
-      <View style={{ width:200,height:200, backgroundColor:"lightgreen"}}></View> */}
-      {/* <Image source={logoImg} style={{width:300, height:300}}/> */}
-      <Image source={logoImg} style={{ width: 300, height: 300 }} />
-      <Text>
-        using the image background with the view section using the image
-        background with the view section using the image background with the
-        view section using the image background with the view section using the
-        image background with the view section using the image background with
-        using the image background with the view section using the image
-        background with the view section using the image background with the
-        view section using the image background with the view section using the
-        image background with the view section using the image background with
-        using the image background with the view section using the image
-        background with the view section using the image background with the
-        view section using the image background with the view section using the
-        image background with the view section using the image background with
-        the view section using the image background with the view section{" "}
-      </Text>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 50 }}>
+      <Text>Testing modal</Text>
+      <Button title="Click Me" onPress={()=>setModal(true)} color="mdinightblue"/>
 
-      {/* <ImageBackground source={logoImg} style={{flex:1}}>
-        <Text>the who's ready to sacrifice himself for bringin the justice </Text>
-      </ImageBackground> */}
-      <Image source={logoImg} style={{ width: 300, height: 300 }} />
-    </ScrollView>
+      <Modal
+       visible={modal}
+       onRequestClose={()=> setModal(false)}
+       animationType="slide"
+       presentationStyle="pageSheet"
+       >
+        <View style={{flex:1,backgroundColor:"lightblue",padding:70}}>
+          <Text>Modal Content</Text>
+          <Button title="Close" color='midnightblue' onPress={()=>setModal(false)}/>
+        </View>
+        
+       </Modal>
     </View>
   );
 }
