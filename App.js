1c0f351dffae1c69624d2d22b608772a9d4c8bfd
sinan <ptsinan8590@gmail.com>
2024-03-22 21:01:58 +0530

    Implementing React Native Alert with message display and utilizing array[] for customizing alternative buttons.

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
 import { useState } from "react";
-import { View,ActivityIndicator} from "react-native";
+import { View, Alert, Button } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
-  const [modal,setModal] = useState(false)
+  const [modal, setModal] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 50 }}>
-      <ActivityIndicator/>
-      <ActivityIndicator size='large'/>
-      <ActivityIndicator size='large' color='midnigthblue'/>
-      <ActivityIndicator size='large' color='midnigthblue' animating={false}/>
+      <Button title="Alert" onPress={() => Alert.alert("Invalid Data")} />
+      <Button
+        title="Alert 2"
+        onPress={() => Alert.alert("Invalid Data", "DOB incorrect")}
+      />
+      <Button
+        title="Alert 3"
+        onPress={() =>
+          Alert.alert("Invalid Data", "DOB incorrect", [
+            {
+              text: "Cancel",
+              onPress: () => console.log("cancel Clicked"),
+            },
+            {
+              text: "Ok",
+              onPress: () => console.log("Ok Clicked"),
+            },
+          ])
+        }
+      />
     </View>
   );
 }
