ef7ad941bfe235055a5a44c22e3c22ceea47e7f2
sinan <ptsinan8590@gmail.com>
2024-03-22 20:35:16 +0530

    Implemented ActivityIndicator functionality in React Native with statusBar styling.

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { useState } from "react";
-import { View, Image, ImageBackground, Text, ScrollView ,Modal, Button,StatusBar} from "react-native";
+import { View,ActivityIndicator} from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   const [modal,setModal] = useState(false)
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 50 }}>
-      <StatusBar backgroundColor="lightblue" barStyle={"dark-content"} />
-      {/* we can ues the hidden in the statusBar for hiding the status bar */}
+      <ActivityIndicator/>
+      <ActivityIndicator size='large'/>
+      <ActivityIndicator size='large' color='midnigthblue'/>
+      <ActivityIndicator size='large' color='midnigthblue' animating={false}/>
     </View>
   );
 }
