ddc2e5dcdd063f618bd7c9cb123310cb3abe8b23
sinan <ptsinan8590@gmail.com>
2024-03-22 17:08:33 +0530

    Implement statusBar styling in React Native, exploring its functionality and design options.

--- App.js
+++ App.js
@@ -1,26 +1,13 @@
 import { useState } from "react";
-import { View, Image, ImageBackground, Text, ScrollView ,Modal, Button} from "react-native";
+import { View, Image, ImageBackground, Text, ScrollView ,Modal, Button,StatusBar} from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   const [modal,setModal] = useState(false)
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 50 }}>
-      <Text>Testing modal</Text>
-      <Button title="Click Me" onPress={()=>setModal(true)} color="mdinightblue"/>
-
-      <Modal
-       visible={modal}
-       onRequestClose={()=> setModal(false)}
-       animationType="slide"
-       presentationStyle="pageSheet"
-       >
-        <View style={{flex:1,backgroundColor:"lightblue",padding:70}}>
-          <Text>Modal Content</Text>
-          <Button title="Close" color='midnightblue' onPress={()=>setModal(false)}/>
-        </View>
-        
-       </Modal>
+      <StatusBar backgroundColor="lightblue" barStyle={"dark-content"} />
+      {/* we can ues the hidden in the statusBar for hiding the status bar */}
     </View>
   );
 }
