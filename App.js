4ed071f3feb2ecc3d0a821e680051d43cabfb6ef
sinan <ptsinan8590@gmail.com>
2024-03-23 18:20:35 +0530

    Implemented and utilized the custom React Native component with argument passing; next phase involves further exploration of additional features like cards, etc.

--- App.js
+++ App.js
@@ -1,31 +1,14 @@
 import { useState } from "react";
 import { View, Alert, Button } from "react-native";
+import Greet from "./components/Greet";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   const [modal, setModal] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 50 }}>
-      <Button title="Alert" onPress={() => Alert.alert("Invalid Data")} />
-      <Button
-        title="Alert 2"
-        onPress={() => Alert.alert("Invalid Data", "DOB incorrect")}
-      />
-      <Button
-        title="Alert 3"
-        onPress={() =>
-          Alert.alert("Invalid Data", "DOB incorrect", [
-            {
-              text: "Cancel",
-              onPress: () => console.log("cancel Clicked"),
-            },
-            {
-              text: "Ok",
-              onPress: () => console.log("Ok Clicked"),
-            },
-          ])
-        }
-      />
+      <Greet name="Bruce Wayne"/>
+      <Greet name="Clark kent"/>
     </View>
   );
 }
