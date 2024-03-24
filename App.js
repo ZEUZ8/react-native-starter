2170997918f3deac2ab908501d970d73b44ac009
sinan <ptsinan8590@gmail.com>
2024-03-24 20:20:56 +0530

    Explored React Native stylesheet functionality, migrating existing styles to new style sheets.

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import { useState } from "react";
-import { View, Alert, Button } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import Greet from "./components/Greet";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   const [modal, setModal] = useState(false);
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 50 }}>
-      <Greet name="Bruce Wayne"/>
-      <Greet name="Clark kent"/>
+    <View style={styles.constainer}>
+      <Greet name="Bruce Wayne" />
+      <View style={[styles.box,styles.lightBlue]}>
+        <Text>Lightblue Box</Text>
+      </View>
+      <View style={[styles.box,styles.ligthgreen]}>
+        <Text>Lightgreen Box</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  constainer: { flex: 1, backgroundColor: "plum", padding: 50 },
+  box:{
+    height:100,
+    width:100,
+    padding:10
+  },
+  lightBlue:{backgroundColor:"lightblue"},
+  ligthgreen:{backgroundColor:"lightgreen"}
+});
