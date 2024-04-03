22dd38c818e896b6e026150f29c2ec23143ddd8d
sinan <ptsinan8590@gmail.com>
2024-04-03 23:51:22 +0530

    by this commit explored the box shadow in the react native testing bothe shadow's in the ios and android

--- App.js
+++ App.js
@@ -8,10 +8,10 @@ export default function App() {
   return (
     <View style={styles.constainer}>
       <Greet name="Bruce Wayne" />
-      <View style={[styles.box,styles.lightBlue]}>
+      <View style={[styles.box,styles.lightBlue,styles.boxShadow]}>
         <Text style={{borderRadius:5}}>Lightblue Box</Text>
       </View>
-      <View style={[styles.box,styles.ligthgreen]}>
+      <View style={[styles.box,styles.ligthgreen,styles.androidShadow]}>
         <Text>Lightgreen Box</Text>
       </View>
     </View>
@@ -21,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   constainer: { flex: 1, backgroundColor: "plum", padding: 50 },
   box:{
-    height:100,
-    width:100,
+    height:250,
+    width:250,
 
     paddingVertical:20,
     paddingHorizontal:10,
@@ -30,11 +30,30 @@ const styles = StyleSheet.create({
     borderWidth:2,
     borderColor:"white",
     // borderStyle:"solid"  //this is the default style
-    borderRadius:5 
+    borderRadius:5 ,
     // you could use the border radius for the view and that will work for both android and i phone 
     // but radius for the text component is only work in android
-
   },
+
   lightBlue:{backgroundColor:"lightblue"},
-  ligthgreen:{backgroundColor:"lightgreen"}
+  ligthgreen:{backgroundColor:"lightgreen"},
+
+  boxShadow:{
+    shadowColor:"#33333",
+    shadowOffset:{
+      width:6,
+      height:6,
+    },
+    shadowOpacity:.6,
+    shadowRadius:4
+  },
+  //using the box shadow will only work on the i phone for androd you need to use something else
+
+  androidShadow:{
+    elevation:20
+  },
+  //using the elevation will help us to bring he shadow for the android phones 
+
+
+
 });
